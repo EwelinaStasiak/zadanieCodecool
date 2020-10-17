d429a98040937c8db4e90116cd81f54040564dad
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2020-10-17 16:46:08 +0200

    1

---
+++ Python_fstring.py
@@ -0,0 +1,5 @@
+
+result = 10
+print(f"Mój wynik z egzaminu to {result / 2.0}, a z praktyk {4.5}")
+print("Mój wynik z egzaminu to", result, ", a z praktyk", 4.5 * 3 / 3.5)
+print("Mój wynik z egzaminu to " + str(result/2.0) + ", a z praktyk " + str(4.5 * 3 / 3.5))
